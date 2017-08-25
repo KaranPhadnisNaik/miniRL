3de1fbd857c395bc85640414588a63401ee85436
karanphadnisnaik <Karanphadnis1@gmail.com>
2017-08-25 08:45:15 -0700

    moved gitignore

--- server/api.py
+++ server/api.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 #from flask_jwt import JWT, jwt_required
 import sqlite3
+from hashing import ShortURL
+from tables import Tables
 
 
 
@@ -29,16 +31,26 @@ class CreateLinks(Resource):
     # post
     def post(self):
         data = CreateLinks.parser.parse_args()
-        # generate hashed value
-        hashedValue = "ljweqwe"
-        item = {'hash': hashedValue , 'full_url': data['full_url']}
+        # store the link in the sql TABLE
+        linksTable = Tables()
+        shortener = ShortURL()
+
+        # insert URL and return _id
+        _id = linksTable.storeURL(data['full_url'])
+
+        # generate hashed value from _id
+        _hash = shortener.endcode(_id)
+
+        # store hash into based on _id
+        linksTable.storeURL(_hash, data['full_url'])
+        item = {'hash': _hash , 'full_url': data['full_url']}
         return item, 201
 
 class HashesAPI(Resource):
     def get(self, slug):
         # check if slug EXISTS
         # if not, give error message
-        # if it does , send the full url and update service
+        # if it does , send the full url and update views and other features
         slug
         return {'items': items}
 
